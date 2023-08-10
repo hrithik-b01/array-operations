4189a661f6b5bc5a6c0b6f9bcddb09f327703569
hrithik-b01 <hrithik.b@codecraft.co.in>
2023-08-10 15:08:39 +0530

    feat:implemented factorial chain function

---
+++ array.js
@@ -0,0 +1,19 @@
+/**
+ * This function checks if an array is a factor chain or not
+ * Example of factor array - [2,4,8,16]
+ * Example of not a factor array - [2,4,8,28]
+ * @param {array} arr 
+ * @returns the result- True or false.
+ */
+
+function factrChain(arr){
+     
+    const array = arr;
+    let flag = true;
+    for(let i=0;i < array.length -1;i++){
+        if(! (array[i+1] % array[i] === 0)){
+            flag = false;
+        }
+    }
+    return flag;
+}
