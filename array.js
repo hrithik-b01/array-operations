834d3a88abf39d6a45257b78345abce3237f185b
KrithikaSuvarna01 <krithika.suvarna@codecraft.co.in>
2023-08-10 15:20:54 +0530

    fix: Bug fixed array.sort() method is replaced with reverse sorting

--- array.js
+++ array.js
@@ -1,8 +1,18 @@
 function array_largest(arr){
-    arr.sort();
-    let largest=arr[arr.length-1];
+    var temp=0;
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length-1;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp
+            }
+        }
+    }
+    console.log(arr);
+    let largest=arr[0];
     return largest;
 }
 
-var large=array_largest([20,70,60,30,50,80])
+var large=array_largest([1,11,2,13,12,14])
 console.log(`The largest number is ${large}`)
